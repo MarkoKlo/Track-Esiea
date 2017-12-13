632551f5ab55339eaeb794cb500659a42ef8170c
AntonLsp <32941176+AntonLsp@users.noreply.github.com>
2017-12-13 10:45:06 +0100

    Additions au client_test

--- trackesiea/trackesiea/client_test.cpp
+++ trackesiea/trackesiea/client_test.cpp
@@ -1,18 +1,21 @@
-using namespace std;
 #include "client_test.h"
 #include<iostream>
+
+using namespace std;
 
 client_test::client_test()
 {
 }
 
-
 client_test::~client_test()
 {
 }
 
-void main()
+int main(int argc, char** argv)
 {
 	cout << "Track'ESIEA est un pst de 2A qui vise a effectuer le tracking 3D d'un objet dans un espace delimite." << endl;
+	cout << "Librairie utilisée : OpenCV." << endl;
+	cout << "Un grand merci à Antoine pour avoir créé le projet." << endl;
 	system("pause");
+	return 0;
 }
