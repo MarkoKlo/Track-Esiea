6b558b3e20b9f951e763f43b8a5a02ada547173e
AntonLsp <32941176+AntonLsp@users.noreply.github.com>
2017-12-13 11:18:14 +0100

    ajout de commentaires

--- trackesiea/trackesiea/client_test.cpp
+++ trackesiea/trackesiea/client_test.cpp
@@ -13,7 +13,8 @@ client_test::~client_test()
 
 int main(int argc, char** argv)
 {
-	cout << "Track'ESIEA est un pst de 2A qui vise a effectuer le tracking 3D d'un objet dans un espace delimite." << endl;
+	// cout est le flux de texte du terminal, '<<' permet d'injecter un élément dans cout, endl correspond à la fin de ligne
+	cout << "Track'ESIEA est un pst de 2A qui vise a effectuer le tracking 3D d'un objet dans un espace delimite." << endl; 
 	cout << "Librairie utilisée : OpenCV." << endl;
 	cout << "Un grand merci à Antoine pour avoir créé le projet." << endl;
 	system("pause");
