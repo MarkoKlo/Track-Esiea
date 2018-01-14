4d640926a5b7582ae638120d1d4f3de4dfb9c6dd
toto5100 <lesparre.antoine@bbox.fr>
2018-01-14 19:03:14 +0100

    Ajout de la librairie OpenCV et des link

--- trackesiea/trackesiea/client_test.cpp
+++ trackesiea/trackesiea/client_test.cpp
@@ -1,3 +1,4 @@
+#include<opencv2/core/core.hpp>
 #include "client_test.h"
 #include<iostream>
 
@@ -15,7 +16,7 @@ int main(int argc, char** argv)
 {
 	cout << "Track'ESIEA est un pst de 2A qui vise a effectuer le tracking 3D d'un objet dans un espace delimite." << endl;
 	cout << "Librairie utilisée : OpenCV." << endl;
-	cout << "Un grand merci à Antoine pour avoir créé le projet." << endl;
+	cout << "Un grand merci a Antoine pour avoir cree le projet." << endl;
 	system("pause");
 	return 0;
 }
